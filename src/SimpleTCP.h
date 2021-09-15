62573e279fe9a9182c6ce0784e2487e70d04e9e0
CompaxChris <57359231+CompaxChris@users.noreply.github.com>
2021-09-15 15:13:13 +0200

    small update

--- src/SimpleTCP.h
+++ src/SimpleTCP.h
@@ -13,7 +13,7 @@ class SimpleTCP
 {
     public:
         SimpleTCP(char* ssid , char* password);
-        void begin(char mDNS[] = "esp32");
+        void begin(char mDNS[]);
         void println(char newString[]);
         void print(char newString[]);
         void checkConnection();
